f2c200b360d6a98147b77596ffc7ac5a7b0e76e2
MAUGIN Thomas <Thom-x@users.noreply.github.com>
2020-12-16 12:46:42 +0100

    feat: add multi platform build docker image (#36)
    * feat: add multi platform build docker image
    
    * chore: add cache and use docker build push action
    
    * chore: add armv6l arch

--- build/fr24feed.sh
+++ build/fr24feed.sh
@@ -6,6 +6,10 @@ case $arch in
     url=https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${FR24FEED_VERSION}_armhf.tgz
     dirname=fr24feed_armhf
     ;;
+  armv6l)
+    url=https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${FR24FEED_VERSION}_armhf.tgz
+    dirname=fr24feed_armhf
+    ;;
   *)
     url=https://repo-feed.flightradar24.com/linux_x86_64_binaries/fr24feed_${FR24FEED_VERSION}_amd64.tgz
     dirname=fr24feed_amd64
