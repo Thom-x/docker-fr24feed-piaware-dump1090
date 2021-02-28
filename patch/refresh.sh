396c51c2685bf466fdd0bfa3a71728bb8b6cc35c
Emilien Klein <emilien@klein.st>
2021-02-28 18:54:19 +0100

    Move the extracted upstream files one level up So as to not keep the version number in the path

--- patch/refresh.sh
+++ patch/refresh.sh
@@ -15,18 +15,21 @@ rm -rf $MODIFIED
 mkdir -p $UPSTREAM
 
 echo "Downloading flightaware/dump1090 release ${DUMP1090_VERSION}"
-wget -O /tmp/flightaware_dump1090.tar.gz $URL
-tar xzf /tmp/flightaware_dump1090.tar.gz --directory=$UPSTREAM
-rm /tmp/flightaware_dump1090.tar.gz
+wget -O /tmp/flightaware_dump1090_${DUMP1090_VERSION}.tar.gz $URL
+tar xzf /tmp/flightaware_dump1090_${DUMP1090_VERSION}.tar.gz --directory=$UPSTREAM
+rm /tmp/flightaware_dump1090_${DUMP1090_VERSION}.tar.gz
+# Move the upstream files one level up, so as to not keep the version number in the path
+mv $UPSTREAM/dump1090-*/* $UPSTREAM
+rm -r $UPSTREAM/dump1090-*/
 # Copy the freshly downloaded release files to the `modified` folder to apply the patches to
 cp -r $UPSTREAM $MODIFIED
 
 echo "Modifying the web page to include Flightradar24 elements"
 # Copy the FR24 logo to the images folder in the `modified` folder
-cp $DIR/resources/fr24-logo.svg $DIR/modified/dump1090-3.8.1/public_html/images
+cp $DIR/resources/fr24-logo.svg $DIR/modified/public_html/images
 # Patch the files in the `modified` folder
 patch -p1 -ru -d $DIR/modified < $DIR/flightradar24.patch
 
 # Done -- See README.md file if the previous command failed, could happen if upstream changed the same lines in their latest release
 echo "\nFiles succesfully patched, open this file in your webbrowser to see the changes:"
-echo "$MODIFIED/dump1090-3.8.1/public_html/index.html"
+echo "$MODIFIED/public_html/index.html"
