31c0ed2e8841e6292192a35136ccc9ac5c2d17c4
Thomas MAUGIN <maugin.thomas@gmail.com>
2021-07-19 10:17:11 +0200

    fix(html): patch error

--- patch/refresh.sh
+++ patch/refresh.sh
@@ -54,7 +54,7 @@ echo "Modifying the web page to include Flightradar24 elements"
 # Copy the FR24 logo to the images folder in the `modified` folder
 cp $DIR/resources/fr24-logo.svg $DIR/modified/public_html_merged/images
 # Patch the files in the `modified` folder
-patch -p1 -ru --force -d $DIR/modified < $DIR/flightradar24.patch
+patch --ignore-whitespace -p1 -ru --force -d $DIR/modified < $DIR/flightradar24.patch
 
 # Done -- See README.md file if the previous command failed, could happen if upstream changed the same lines in their latest release
 if [ $? -eq 0 ]
