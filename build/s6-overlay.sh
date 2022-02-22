c7065ffca9907587ef8f019550e309410c2096b1
Thomas MAUGIN <maugin.thomas@gmail.com>
2022-02-22 22:23:02 +0100

    chore(s6-overlay): use s6-overlay v3, use the new way to handles services etc for it

--- build/s6-overlay.sh
+++ build/s6-overlay.sh
@@ -3,22 +3,24 @@ arch=$(dpkg --print-architecture)
 
 case $arch in
   armhf)
-    url=https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-armhf.tar.gz
+    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-armhf-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   arm64)
-    url=https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-aarch64.tar.gz
+    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-aarch64-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   armel)
-    url=https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-arm.tar.gz
+    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-arm-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   amd64)
-    url=https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-amd64.tar.gz
+    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-x86_64-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   *)
     exit 1
     ;;
 esac
 
+wget -O /tmp/s6-overlay-noarch.tar.gz https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch-${S6_OVERLAY_VERSION}.tar.xz /tmp
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.gz && rm /tmp/s6-overlay-noarch.tar.gz
 wget -O /tmp/s6-overlay.tar.gz $url && \
-tar xzf /tmp/s6-overlay.tar.gz -C / && rm /tmp/s6-overlay.tar.gz
+tar -C / -Jxpf /tmp/s6-overlay.tar.gz && rm /tmp/s6-overlay.tar.gz
 
