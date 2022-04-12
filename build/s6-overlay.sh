cd0c7974504b92281f1070d7c8b6432883925282
Thomas MAUGIN <maugin.thomas@gmail.com>
2022-04-12 16:16:39 +0200

    fix(build): fix s6 overlay version for armel arch

--- build/s6-overlay.sh
+++ build/s6-overlay.sh
@@ -9,7 +9,7 @@ case $arch in
     url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-aarch64-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   armel)
-    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-arm-${S6_OVERLAY_VERSION}.tar.xz
+    url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-armhf-${S6_OVERLAY_VERSION}.tar.xz
     ;;
   amd64)
     url=https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-x86_64-${S6_OVERLAY_VERSION}.tar.xz
